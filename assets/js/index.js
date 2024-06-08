53fd293729507cb6f8703d506ff2e6d7a91d83dd
Barbara Estrada <barbaraestrada.cs@gmail.com>
2024-06-08 15:05:14 -0400

    Mejora Variables

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 /* Precio Base*/
-let precio = 400000;
-
-document.querySelector("#precio-base").innerHTML = precio;
-
+const precioBase = 400000;
 let cantidad = 0;
+let totalPagar = 0;
+
+document.querySelector("#precio-base").innerHTML = precioBase;
+document.querySelector("#cantidad").innerHTML = cantidad;
+document.querySelector("#valor-total").innerHTML = totalPagar;
